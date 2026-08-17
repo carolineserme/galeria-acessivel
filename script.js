92f71376a47ca36dcc9ca7bb7176f0d7adb116b0
carolineserme <carolineserme22@gmail.com>
2026-08-17 18:28:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function upDate(previewPic) {
+  const displayBox = document.getElementById('image');
+  displayBox.innerText = previewPic.alt;
+  displayBox.style.backgroundImage = `url('${previewPic.src}')`;
+}
+
+function unDo() {
+  const displayBox = document.getElementById('image');
+  displayBox.innerText = "Passe o mouse ou navegue com a tecla Tab sobre uma imagem abaixo.";
+  displayBox.style.backgroundImage = "url('')";
+}
+
+function setTabFocus() {
+  console.log("Evento onload acionado: configurando tabIndex para acessibilidade.");
+  const images = document.querySelectorAll('.preview');
+  for (let i = 0; i < images.length; i++) {
+    images[i].setAttribute('tabindex', '0');
+  }
+}
+
+window.onload = setTabFocus;
