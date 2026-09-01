74507bc316f54df964acdc3393db183643131ba2
carolineserme <carolineserme22@gmail.com>
2026-09-01 17:04:02 -0300

    Refactor upDate and unDo functions to use innerText

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 function upDate(previewPic) {
+
   const displayBox = document.getElementById('image');
-  displayBox.innerHTML = `<span>${previewPic.alt}</span>`;
+
+  displayBox.innerText = previewPic.alt;
+
   displayBox.style.backgroundImage = `url('${previewPic.src}')`;
+
 }
+
+
 
 function unDo() {
+
   const displayBox = document.getElementById('image');
-  displayBox.innerHTML = `<span>Passe o mouse ou navegue com a tecla Tab sobre uma imagem abaixo.</span>`;
+
+  displayBox.innerText = "Passe o mouse ou navegue com a tecla Tab sobre uma imagem abaixo.";
+
   displayBox.style.backgroundImage = "url('')";
+
 }
+
+
 
 function setTabFocus() {
+
   console.log("Evento onload acionado: configurando tabIndex para acessibilidade.");
+
   const images = document.querySelectorAll('.preview');
+
   for (let i = 0; i < images.length; i++) {
+
     images[i].setAttribute('tabindex', '0');
+
   }
+
 }
 
-window.onload = setTabFocus;
+
+
+window.onload = setTabFocus;  
+
